bc5b83b545f4e0f151afa2e4c1953d6f028eb160
marcioespindolamge-star <marcioespindola.mge@gmail.com>
2026-09-20 08:10:44 -0300

    Preparar integração segura do SIGAP com Firebase

---
+++ static/firebase-config.js
@@ -0,0 +1,10 @@
+// SIGAP - configuração pública do Firebase Web SDK
+// Projeto: sigap-agropecuaria-paineira
+export const firebaseConfig = {
+  apiKey: "AIzaSyDRudP9uXrhCabfPaOce2QnZpu19OB8JFA",
+  authDomain: "sigap-agropecuaria-paineira.firebaseapp.com",
+  projectId: "sigap-agropecuaria-paineira",
+  storageBucket: "sigap-agropecuaria-paineira.firebasestorage.app",
+  messagingSenderId: "834359371662",
+  appId: "1:834359371662:web:3c7e9831ad93832c35a6df"
+};
